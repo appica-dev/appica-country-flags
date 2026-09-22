a858eaeda6487db1943158f95ffbd396dd06fe7f
Dmytro Kovalov <chop.suet@gmail.com>
2026-09-22 22:53:44 +0200

    chore: stop tracking generated flag components

--- packages/react/src/flags/rounded/it.tsx
+++
@@ -1,3 +0,0 @@
-import { createCountryFlag } from "../../createCountryFlag.js";
-const ITRounded = createCountryFlag("it", "<path fill=\"#f0f0f0\" d=\"M40 28c0 6.627-5.373 12-12 12H12C5.373 40 0 34.627 0 28V12C0 5.373 5.373 0 12 0h16c6.627 0 12 5.373 12 12z\"/><path fill=\"#d80027\" fill-rule=\"evenodd\" d=\"M27 0h1c6.627 0 12 5.373 12 12v16c0 6.627-5.373 12-12 12h-1z\" clip-rule=\"evenodd\"/><path fill=\"#6da544\" fill-rule=\"evenodd\" d=\"M12 0h1v40h-1C5.373 40 0 34.627 0 28V12C0 5.373 5.373 0 12 0\" clip-rule=\"evenodd\"/>");
-export default ITRounded;
